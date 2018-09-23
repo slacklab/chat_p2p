b2c70b074bae10932216a58ec43c1f9da5b3c09e
Ivan Sorokoletov <feltramt@gmail.com>
2018-09-24 00:29:40 +0300

    Some fixes, add HelperFunctions.swift

---
+++ tChat/tChat/Helpers/HelperFunctions.swift
@@ -0,0 +1,13 @@
+//
+//  HelperFunctions.swift
+//  tChat
+//
+//  Created by z on 23/09/2018.
+//  Copyright © 2018 Ivan Sorokoletov. All rights reserved.
+//
+
+import Foundation
+
+func printState(_: String) {
+    print("Function: \(#function), line: \(#line)")
+}
